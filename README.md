413f4efeee1dfe13f5a2a2377cdaec214c5b986c
GuillaumeBchd <guillaumebouchard@orange.fr>
2020-12-24 16:15:10 +0100

    Added general structure of main

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Polaris
 
-Polaris is a simple authentication server using OAuth2.0 and OpenId Connect. It is written in Go and uses data from a MongoDB database.
+Polaris is a simple authentication server using OAuth2.0 and OpenId Connect. It is written in Go and uses data from a Postgres database.
 
 This project is mainly to practice coding in Go and to learn some security stuff.
--- cmd/polaris/main.go
+++ cmd/polaris/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/authorize", NotImplemented).Methods("GET")
+	r.HandleFunc("/token", NotImplemented).Methods("POST")
+
+	r.HandleFunc("/login", NotImplemented).Methods("GET")
+	r.HandleFunc("/login", NotImplemented).Methods("POST")
+
+	r.HandleFunc("/register", NotImplemented).Methods("GET")
+	r.HandleFunc("/register", NotImplemented).Methods("POST")
+
+	r.HandleFunc("/client", NotImplemented).Methods("GET")
+	r.HandleFunc("/client", NotImplemented).Methods("POST")
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Println("Starting server on : " + srv.Addr)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func NotImplemented(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
 }
